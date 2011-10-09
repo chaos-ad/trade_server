13e2cfe9259db79b8139c7d862414baf08eca0b7
Anatoly Kanivetsky <chaos-ad@yandex.ru>
2011-10-09 23:08:54 +0400

    forgotten file

---
+++ include/trade_terminal_state.hrl
@@ -0,0 +1,38 @@
+-record(server_status, {
+    id,
+    connected,
+    recover
+}).
+
+-record(client,  {
+    type,
+    currency,
+    ml_call,
+    ml_close,
+    ml_intraday,
+    ml_overnight,
+    ml_restrict
+}).
+
+-record(market, {id, name}).
+-record(candle, {id, period, name}).
+
+-record(security, {
+    secid,
+    sectype,
+    active,
+    seccode,
+    market,
+    shortname,
+    decimals,
+    minstep,
+    lotsize,
+    point_cost,
+    usecredit,
+    bymarket,
+    nosplit,
+    immorcancel,
+    cancelbalance
+}).
+
+-record(terminal_state, {server_status, client, markets, securities, candles}).
