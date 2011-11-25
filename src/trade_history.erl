2d350ccadfc605028d744d110251e96c5f6b4f2c
Anatoly Kanivetsky <Chaos.A.D88@gmail.com>
2011-11-25 14:09:36 +0400

    History downloading improved
    Never download history for the current day.
    Never download history for the unclosed bar, if timeframe is greater than one day.

--- src/trade_history.erl
+++ src/trade_history.erl
@@ -1,6 +1,8 @@
 -module(trade_history).
 -compile(export_all).
 
+-include("trade_periods.hrl").
+
 -define(SERVER, {global, ?MODULE}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -9,10 +11,11 @@ start_link() ->
     gen_server:start_link(?SERVER, ?MODULE, [], []).
 
 get_history(Symbol, Period, From) ->
-    get_history(Symbol, Period, From, trade_utils:local_time()).
+    get_history(Symbol, Period, From, last_closed_bar(Period)).
 
 get_history(Symbol, Period, From, To) ->
-    gen_server:call(?SERVER, {get_history, Symbol, Period, From, To}, infinity).
+    {T1, T2} = get_range(Period, From, To),
+    gen_server:call(?SERVER, {get_history, Symbol, Period, T1, T2}, infinity).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -28,30 +31,26 @@ init([]) ->
     end,
     {ok, undefined}.
 
-handle_call({get_history, Symbol, Period, From, To}, _, State) ->
-
-    T1 = trade_utils:to_date(From),
-    T2 = min( trade_utils:to_date(To), local_yesterday() ),
-
+handle_call({get_history, Symbol, Period, T1, T2}, _, State) ->
     case trade_db:get_range(Symbol, Period) of
         undefined ->
             fetch_history(Symbol, Period, T1, T2),
-            trade_db:set_range(Symbol, Period, T1, T2),
-            {reply, trade_db:get_history(Symbol, Period, From, To), State};
-        {Begin, End} ->
-            fetch_history(Symbol, Period, T1, Begin),
-            fetch_history(Symbol, Period, End, T2),
-            trade_db:set_range(Symbol, Period, min(Begin, T1), max(End, T2)),
-            {reply, trade_db:get_history(Symbol, Period, From, To), State}
+            trade_db:set_range(Symbol, Period, {T1, T2}),
+            {reply, trade_db:get_history(Symbol, Period, T1, T2), State};
+        {HistT1, HistT2} ->
+            fetch_history(Symbol, Period, T1, HistT1),
+            fetch_history(Symbol, Period, HistT2, T2),
+            trade_db:set_range(Symbol, Period, {min(HistT1, T1), max(HistT2, T2)}),
+            {reply, trade_db:get_history(Symbol, Period, T1, T2), State}
     end.
 
-fetch_history(Symbol, Period, From, To) when From < To ->
+fetch_history(Symbol, Period, T1, T2) when T1 < T2 ->
     {MarketID, SymbolID} = get_symbol_info(Symbol),
-    Data = trade_finam:download_history(MarketID, SymbolID, Period, From, To),
+    Data = trade_finam:download_history(MarketID, SymbolID, Period, T1, T2),
     lager:debug("Importing ~B bars...", [length(Data)]),
     ok = trade_db:add_history(Symbol, Period, Data);
 
-fetch_history(_, _, From, To) when From >= To -> ok.
+fetch_history(_, _, T1, T2) when T1 >= T2 -> ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -61,6 +60,16 @@ get_symbol_info(Symbol) ->
         _                    -> exit("symbol not found")
     end.
 
+get_range(Period, T1, T2) ->
+    D1 = trade_utils:to_date(T1),
+    D2 = trade_utils:to_date(T2),
+    LastBar = last_closed_bar(Period),
+    case D1 > D2 orelse D1 > LastBar orelse D2 > LastBar of
+        true  -> exit(invalid_range);
+        false -> {D1, D2}
+    end.
 
-local_yesterday() ->
-    trade_utils:to_date(trade_utils:sub_days(trade_utils:local_time(), 1)).
+last_closed_bar(?PERIOD_W1)  -> edate:shift(-calendar:day_of_the_week(edate:today()), day);
+last_closed_bar(?PERIOD_MN1) -> edate:end_of_month(edate:shift(-1, month));
+last_closed_bar(_)           -> edate:yesterday().
+
