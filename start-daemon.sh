bce401f804ee686d6fa0a4143e2553cc823a6031
Anatoly Kanivetsky <Chaos.A.D88@gmail.com>
2011-12-27 13:43:23 +0400

    application renamed to trade_server. trade_admin is now logs all it's requests

--- start-daemon.sh
+++ start-daemon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`
-exec erl -pa ebin deps/*/ebin -boot start_sasl -config priv/app.config -sname tradesrv@`hostname` -cookie trade_cookie -detached -s trade
+exec erl -pa ebin deps/*/ebin -boot start_sasl -config priv/app.config -sname tradesrv@`hostname` -cookie trade_cookie -detached -s trade_server
