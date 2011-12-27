218d161e3193bf2cd499bde5d882a705f5d47bc7
Anatoly Kanivetsky <Chaos.A.D88@gmail.com>
2011-12-27 13:44:27 +0400

    Renamed files added

---
+++ src/trade_server.erl
@@ -0,0 +1,31 @@
+-module(trade_server).
+
+-export([start/0, stop/0]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+start() ->
+    start(?MODULE).
+
+start(App) ->
+    start_ok(App, application:start(App, permanent)).
+
+stop() ->
+    application:stop(?MODULE).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+start_ok(_, ok) ->
+    ok;
+
+start_ok(_, {error, {already_started, _App}}) ->
+    ok;
+
+start_ok(App, {error, {not_started, Dep}}) when App =/= Dep ->
+    ok = start(Dep),
+    start(App);
+
+start_ok(App, {error, Reason}) ->
+    erlang:error({app_start_failed, App, Reason}).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ src/trade_server_sup.erl
@@ -0,0 +1,29 @@
+-module(trade_server_sup).
+-behaviour(supervisor).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-export([start_link/0, init/1]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-define(CHILD(I), {I, {I, start_link, []}, permanent, 60000, worker, [I]}).
+-define(CHILD(I, Args), {I, {I, start_link, Args}, permanent, 60000, worker, [I]}).
+-define(CHILD(I, Args, Role), {I, {I, start_link, Args}, permanent, 60000, Role, [I]}).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+init([]) ->
+    {ok, { {one_for_one, 5, 60}, [
+        ?CHILD(trade_db),
+        ?CHILD(trade_history),
+        ?CHILD(trade_strategy_mgr, [], supervisor),
+        ?CHILD(trade_terminal_mgr, [], supervisor)
+    ]} }.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
